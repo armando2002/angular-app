44a20bc41e37aea71b53527b32e8afb66b96cbf4
Stephen Williams <armando2002@gmail.com>
2019-02-02 15:29:44 -0800

    keyboard & input events

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,62 +8,64 @@ import { User } from '../../models/user';
 })
 export class UsersComponent implements OnInit {
   users: User[];
-  showExtended: boolean = false;
+  showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
-      this.users = [
-        {
-          firstName: 'John',
-          lastName: 'Doe',
-          age: 70,
-          address: {
-            street: '50 Main st',
-            city: 'Boston',
-            state: 'MA'
-          },
-          isActive: true,
-          registered: new Date('01/02/2018 08:30:00')
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 70,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA'
         },
-        {
-          firstName: 'Kevin',
-          lastName: 'Johnson',
-          age: 34,
-          address: {
-            street: '20 School St',
-            city: 'Lynn',
-            state: 'MA'
-          },
-          isActive: false,
-          registered: new Date('03/11/2017 06:20:00')
+        isActive: true,
+        registered: new Date('01/02/2018 08:30:00'),
+        hide: true
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School St',
+          city: 'Lynn',
+          state: 'MA'
         },
-        {
-          firstName: 'Karen',
-          lastName: 'Williams',
-          age: 26,
-          address: {
-            street: '55 Mill st',
-            city: 'Miami',
-            state: 'FL'
-          },
-          isActive: true,
-          registered: new Date('11/02/2016 10:30:00')
-        }
-      ];
+        isActive: false,
+        registered: new Date('03/11/2017 06:20:00'),
+        hide: true
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill st',
+          city: 'Miami',
+          state: 'FL'
+        },
+        isActive: true,
+        registered: new Date('11/02/2016 10:30:00'),
+        hide: true
+      }
+    ];
 
     this.loaded = true;
   }
 
-addUser(user: User) {
-  this.users.push(user);
-}
+  addUser(user: User) {
+    this.users.push(user);
+  }
 
-fireEvent(e) {
-/*   console.log('Button clicked'); */
-  console.log(e.type);
-}
+  /* toggleHide(user: User) {
+    user.hide = !user.hide;
+  } */
 
 }
