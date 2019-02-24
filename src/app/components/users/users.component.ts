04e6ef94a72f5bd83321fbec88ee7b0ce0a60706
Stephen Williams <armando2002@gmail.com>
2019-02-23 16:57:38 -0800

    section 6 lecture 32 what is a service?

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { User } from '../../models/user';
 
@@ -18,6 +18,7 @@ export class UsersComponent implements OnInit {
   loaded: boolean = false;
   enableAdd: boolean = false;
   showUserForm: boolean = false;
+  @ViewChild('userForm') form: any;
 
   constructor() { }
 
@@ -65,9 +66,19 @@ export class UsersComponent implements OnInit {
   }
   } */
 
-  onSubmit(e) {
-    console.log(123);
-    e.preventDefault();
+  onSubmit({value, valid}: {value: User, valid: boolean}) {
+    if(!valid) {
+      console.log('Form is not valid');
+    }
+    else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
+
+      this.users.unshift(value);
+
+      this.form.reset();
+    }
   }  
 
 }
